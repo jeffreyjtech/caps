8807e65bd02c42bd0773d8593cc33dd0075660c6
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-04 20:48:36 -0700

    Added hub file which has test timer event

--- hub.js
+++ hub.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const Chance = require('chance');
+const chance = new Chance();
+
+const eventPool = require('./event-pool');
+const logEvent = require('./lib/event-logger');
+const { handlePickup } = require('./lib/driver');
+const { pickupEmitter, deliveredListener } = require('./lib/vendor');
+
+eventPool.on('PICKUP', (payload) => logEvent(payload, 'PICKUP'));
+eventPool.on('IN-TRANSIT', (payload) => logEvent(payload, 'IN-TRANSIT'));
+eventPool.on('DELIVERED', (payload) => logEvent(payload, 'DELIVERED'));
+
+eventPool.on('PICKUP', handlePickup);
+eventPool.on('DELIVERED', deliveredListener);
+
+
+setInterval(() => {
+  let randomStore = chance.company();
+  pickupEmitter(randomStore);
+}, 3000);
