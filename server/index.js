e7b8e6d973daec7619fc8bc4c8cba2e86ba79e13
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-05 21:40:10 -0700

    Added wiring for relaying events

--- server/index.js
+++ server/index.js
@@ -7,8 +7,23 @@ const PORT = process.env.PORT || 3000;
 const server = new Server(PORT);
 const caps = server.of('/caps');
 
+const addLogger = require('./event-logger');
+
 caps.on('connection', socket => {
 
+  console.log('Poggers! Socket connected!', socket.id);
+
+  addLogger(socket);
+
+  socket.on('PICKUP', (payload) => {
+    console.log('Relaying PICKUP');
+    socket.broadcast.emit('PICKUP', payload);
+  });
+
+  socket.on('DELIVERED', (payload) => {
+    console.log('Relaying DELIVERED');
+    socket.broadcast.emit('DELIVERED', payload);
+  });
 });
 
 console.log('caps.on() executed, server should be up');
