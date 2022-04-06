e00b14c2a4b1b8926b8e873c8b000d6b5f220c07
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-05 20:13:05 -0700

    Added mock eventPool dependency

--- __tests__/event-logger.test.js
+++ __tests__/event-logger.test.js
@@ -8,6 +8,12 @@ const addLogger = require('../lib/event-logger.js');
 const eventPool = require('../event-pool');
 
 // Mock objects and spy functions
+jest.mock('../event-pool', () => {
+  return {
+    on: jest.fn(),
+    emit: jest.fn(),
+  };
+});
 console.log = jest.fn();
 // jest.spyOn(console, 'log');
 
@@ -22,15 +28,20 @@ describe('Testing event logger', () => {
       address: chance.address(),
     };
     let testEventName = 'PICKUP';
-    // timestamp has to be generated last so the test passes.
-    // let timestamp = new Date();
-
     addLogger(eventPool, testEventName);
 
-    let listener = eventPool.listeners(testEventName)[0];
+    // The next line stores the arguments when eventPool.on() was called
+    // .mock is the mock property provided by jest, which then has a .calls property which is an array
+    // the .calls array contains sub-arrays of each argument for each call of .on()
+    let [ mockCall ] = eventPool.on.mock.calls;
 
-    listener(testPayload);
+    // This finds the callback so we can test it
+    let loggerCallback = mockCall.find((argument) => typeof argument === 'function');
 
+    // Then we execute the callback to see that it calls console.log in some capacity
+    loggerCallback(testPayload);
     expect(console.log).toBeCalled();
+    // Finally we test that 'PICKUP' was also among the arguments
+    expect(mockCall.find((argument) => argument === testEventName)).toBeTruthy();
   });
 });
