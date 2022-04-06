8547682eae281766ad94d2ec67a13b9b3724e0e9
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-05 22:33:57 -0700

    Refactored event logger test for socket-io

--- __tests__/event-logger.test.js
+++ __tests__/event-logger.test.js
@@ -1,17 +1,14 @@
 'use strict';
 
 const addLogger = require('../server/event-logger.js');
-const eventPool = require('../event-pool');
+// const eventPool = require('../server/event-pool');
 
 // Mock objects and spy functions
-jest.mock('../event-pool', () => {
-  return {
-    on: jest.fn(),
-    emit: jest.fn(),
-  };
-});
-console.log = jest.fn();
-// jest.spyOn(console, 'log');
+const socket = {
+  onAny: jest.fn(),
+};
+// console.log = jest.fn();
+jest.spyOn(console, 'log');
 
 describe('Testing event logger', () => {
 
@@ -20,20 +17,19 @@ describe('Testing event logger', () => {
       nothing: 'in-particular',
     };
     let testEventName = 'PICKUP';
-    addLogger(eventPool, testEventName);
-
-    // The next line stores the arguments when eventPool.on() was called
+    addLogger(socket);
+    
+    // The next line stores the arguments when socket.onAny() was called
     // .mock is the mock property provided by jest, which then has a .calls property which is an array
-    // the .calls array contains sub-arrays of each argument for each call of .on()
-    let mockCallArgs = eventPool.on.mock.calls[0];
+    // the .calls array contains sub-arrays of each argument for each call of .onAny()
+    let mockCallArgs = socket.onAny.mock.calls[0];
 
     // This finds the callback so we can test it
     let loggerCallback = mockCallArgs.find((argument) => typeof argument === 'function');
 
     // Then we execute the callback to see that it calls console.log in some capacity
-    loggerCallback(testPayload);
+    loggerCallback(testEventName, testPayload);
     expect(console.log).toBeCalled();
     // Finally we test that 'PICKUP' was also among the arguments
-    expect(mockCallArgs.find((argument) => argument === testEventName)).toBeTruthy();
   });
 });
