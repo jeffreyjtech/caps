47493391ed2ca1c6a917c30499d64ad01f2e68bb
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-07 01:47:59 -0700

    Fixed MessageClient to Client

--- client/__tests__/client.test.js
+++ client/__tests__/client.test.js
@@ -2,7 +2,7 @@
 
 const { io } = require('socket.io-client');
 
-const MessageClient = require('../lib/Client');
+const Client = require('../lib/Client');
 
 jest.mock('socket.io-client', () => {
   return {
@@ -19,7 +19,7 @@ describe('Testing the message client module', () => {
   let testClient;
 
   test('Should call socket functions on instantiation', () => {
-    testClient = new MessageClient('test');
+    testClient = new Client('test');
     expect(io).toHaveBeenCalledWith(expect.anything());
     expect(testClient.socket.emit).toHaveBeenCalledWith('join', { queueId: 'test' });
     expect(testClient.socket.emit).toBeCalled();
