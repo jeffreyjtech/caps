1491bf44ff47b220c1f3552c84b5ad91236a5ccd
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-07 01:39:03 -0700

    Updated import path

--- client/__tests__/client.test.js
+++ client/__tests__/client.test.js
@@ -2,7 +2,7 @@
 
 const { io } = require('socket.io-client');
 
-const MessageClient = require('../lib/client');
+const MessageClient = require('../lib/Client');
 
 jest.mock('socket.io-client', () => {
   return {
