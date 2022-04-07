fc776485a0577d15162d52ad2532f07e9ce729c3
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-06 22:51:44 -0700

    Refactored server to use a queue for storing events

--- server/index.js
+++ server/index.js
@@ -8,25 +8,63 @@ const server = new Server(PORT);
 const caps = server.of('/caps');
 
 const addLogger = require('./event-logger');
+const Queue = require('./lib/queue');
+
+const globalQueue = new Queue();
 
 caps.on('connection', socket => {
 
   addLogger(socket);
 
   console.log('Poggers! Socket connected!', socket.id);
-  socket.on('JOIN', (vendorId) => {
-    console.log('Registering Vendor to room ', vendorId);
-    socket.join(vendorId);
+
+  socket.on('join', (payload) => {
+    // The queueIds should NOT be shared between stores
+    console.log('Registering client to room ', payload.queueId);
+    socket.join(payload.queueId);
   });
 
   socket.on('PICKUP', (payload) => {
-    console.log('Relaying PICKUP');
-    socket.broadcast.emit('PICKUP', payload);
+    console.log('Relaying/storing PICKUP');
+
+    let eventQueue = globalQueue.createIfNotExists('driver', new Queue());
+
+    eventQueue.create(payload.messageId, payload);
+
+    socket.to('driver').emit('PICKUP', payload);
   });
 
   socket.on('DELIVERED', (payload) => {
-    console.log('Relaying DELIVERED to room', payload.store);
-    socket.to(payload.store).emit('DELIVERED', payload);
+    console.log('Relaying/storing DELIVERED', payload.store);
+
+    let eventQueue = globalQueue.createIfNotExists(payload.store, new Queue());
+
+    eventQueue.create(payload.messageId, payload);
+
+    socket.to(payload.queueId).emit('DELIVERED', payload);
+  });
+
+  socket.on('received', (payload) => {
+    console.log('A dequeued payload has been acknowledged by a client');
+    let eventQueue = globalQueue.read(payload.queueId);
+
+    if(!eventQueue) throw new Error('Queue does not exist!');
+
+    let message = eventQueue.remove(payload.messageId);
+    console.log('Lifecycle has ended for messageId: ', payload.messageId);
+    caps.emit('received', message);
+  });
+
+  socket.on('get-all', (payload) => {
+    console.log('Getting all events in queueId: ', payload.queueId);
+    let eventQueue = globalQueue.read(payload.queueId);
+
+    console.log(eventQueue);
+
+    eventQueue.readAllValues().forEach((payload) => {
+      console.log('Fetched payload', payload);
+      socket.emit(payload.eventName, payload);
+    });
   });
 });
 
