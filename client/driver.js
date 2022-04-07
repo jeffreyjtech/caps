0a0d6357fb83c379473f43bdc70d90718be055ca
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-06 22:50:59 -0700

    Refactored driver to use Client class

--- client/driver.js
+++ client/driver.js
@@ -1,5 +1,22 @@
 'use strict';
 
+const Client = require('./lib/client');
+const driverClient = new Client('driver');
+
+console.log('Fetching events from server');
+driverClient.publish('get-all', { eventName: 'PICKUP', queueId: 'driver' });
+
+driverClient.subscribe('PICKUP', (payload) => {
+  console.log('DRIVER: picked up', payload.orderID);
+  driverClient.publish('received', payload);
+  driverClient.publish('IN-TRANSIT', payload);
+  console.log('DRIVER: delivered', payload.orderID);
+  driverClient.publish('DELIVERED', payload);
+});
+
+module.exports = driverClient;
+
+/*
 const { io } = require('socket.io-client');
 const socket = io('http://localhost:3000/caps');
 
@@ -11,7 +28,4 @@ const handlePickup = (socket) => (payload) => {
 };
 
 socket.on('PICKUP', handlePickup(socket));
-
-module.exports = {
-  handlePickup,
-};
+*/
