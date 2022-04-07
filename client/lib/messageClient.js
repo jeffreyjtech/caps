5c23fce3b710bfdb71353b2a9f8cf86d5c283047
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-06 21:02:14 -0700

    Created module for MessageClient class

---
+++ client/lib/messageClient.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// This module contains a class for abstracting a client application for our CAPS system
+// On instantiation, the new MessageClient will emit a 'join' event.
+// The server app will join the new Client to a room named after the queueId upon receipt of the 'join' event.
+
+const { io } = require('socket.io-client');
+const SOCKET_URL = process.env.SOCKET_URL || 'http://localhost:3000/caps';
+
+class MessageClient {
+  constructor(queueId) {
+    this.queueId = queueId;
+    this.socket = io(SOCKET_URL);
+    this.socket.emit('join', { queueId });
+    this.socket.on('join', (id) => console.log('Joined queue with id:\n ', id));
+  }
+
+  publish(event, payload) {
+    this.socket.emit(event, { ...payload, queueId: this.queueId });
+  }
+
+  subscribe(event, callback) {
+    this.socket.on(event, callback);
+  }
+}
+
+module.exports = MessageClient;
